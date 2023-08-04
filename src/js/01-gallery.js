740288e977b75c737a52f9486a168ffb734f2dc0
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-04 14:47:25 +0300

    finished task #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({ original, preview, description }) =>
+  `
+    <li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+).join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+
+const simpLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
