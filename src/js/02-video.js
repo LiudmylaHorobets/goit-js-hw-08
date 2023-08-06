7ba827d52b61560c059d065379c7833835729560
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-06 15:30:29 +0300

    finished task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const getCurrentTimeKey = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
 
